338d7897652d24950f0f0b33c4ed19453c82b7ce
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2021-11-18 23:37:13 +0100

    Add function

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@ RG_NAME=elicznik_billing_period_rg
 RG_LOCATION=westeurope 
 FUNCTION_APP_NAME=elicznik-billing-period
 az group create --name $RG_NAME --location $RG_LOCATION
-#az deployment create --name $RG_NAME --parameter rgName=$RG_NAME --location $RG_LOCATION --parameter rgLocation=$RG_LOCATION --parameter functionAppName=$FUNCTION_APP_NAME --template-file azuredeploy.json --parameters @azuredeployparameters.json 
 az group deployment create --resource-group $RG_NAME --template-file azuredeploy.json
