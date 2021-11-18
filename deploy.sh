dc6a97c1fb94c8f8f71ee62881c46806f96010d3
Bartosz Podlejski <bartosz.podlejski@gmail.com>
2021-11-18 19:23:43 +0100

    Rename parameter

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 RG_NAME=elicznik_billing_period_rg 
 RG_LOCATION=westeurope 
-az deployment create --name $RG_NAME --parameter rgName=$RG_NAME --location $RG_LOCATION --parameter rgLocation=$RG_LOCATION --template-file azuredeploy.json --parameters @azuredeployparameters.json
+FUNCTION_APP_NAME=elicznik-billing-period
+az deployment create --name $RG_NAME --parameter rgName=$RG_NAME --location $RG_LOCATION --parameter rgLocation=$RG_LOCATION --parameter functionAppName=$FUNCTION_APP_NAME --template-file azuredeploy.json --parameters @azuredeployparameters.json 
